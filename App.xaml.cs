6a46e055737baaff08a7424815a3d60a5242cf0e
Dan Rogers <danrog729@gmail.com>
2025-02-07 19:23:35 +0000

    made tab control theme style better

--- App.xaml.cs
+++ App.xaml.cs
@@ -37,15 +37,20 @@ namespace noughts_and_crosses
         {
             InitializeComponent();
 
-            themes.Add(new Theme() { Name = "Light", Path = "Themes/Light.xaml" });
-            themes.Add(new Theme() { Name = "Dark", Path = "Themes/Dark.xaml" });
-            themes.Add(new Theme() { Name = "High Contrast Light", Path = "Themes/HighContrastLight.xaml" });
-            themes.Add(new Theme() { Name = "High Contrast Dark", Path = "Themes/HighContrastDark.xaml" });
-            themes.Add(new Theme() { Name = "Colourful", Path = "Themes/Colourful.xaml" });
-            themes.Add(new Theme() { Name = "Industrial", Path = "Themes/Factorio.xaml" });
-            themes.Add(new Theme() { Name = "Space Age", Path = "Themes/Kerbal.xaml" });
-            themes.Add(new Theme() { Name = "Programmer", Path = "Themes/Perry.xaml" });
-            CurrentTheme = themes[0];
+            themes.Add(new Theme("Light", "Themes/Light.xaml"));
+            themes.Add(new Theme("Dark", "Themes/Dark.xaml"));
+            themes.Add(new Theme("High Contrast Light", "Themes/HighContrastLight.xaml"));
+            themes.Add(new Theme("High Contrast Dark", "Themes/HighContrastDark.xaml"));
+            themes.Add(new Theme("Colourful", "Themes/Colourful.xaml"));
+            themes.Add(new Theme("Industrial", "Themes/Factorio.xaml"));
+            themes.Add(new Theme("Space Age", "Themes/Kerbal.xaml"));
+            themes.Add(new Theme("Programmer", "Themes/Perry.xaml"));
+            _currentTheme = themes[0];
+            ResourceDictionary styles = new ResourceDictionary() { Source = new Uri("Styles.xaml", UriKind.Relative) };
+            ResourceDictionary theme = new ResourceDictionary() { Source = new Uri(_currentTheme.Path, UriKind.Relative) };
+            theme.MergedDictionaries.Add(styles);
+            Resources.MergedDictionaries.Clear();
+            Resources.MergedDictionaries.Add(theme);
 
             soundsOn = true;
             moveSound = new Sound("pack://application:,,,/Themes/Sounds/move.wav");
@@ -58,6 +63,12 @@ namespace noughts_and_crosses
     {
         public string Name;
         public string Path;
+
+        public Theme(string name, string path)
+        {
+            Name = name;
+            Path = path;
+        }
     }
 
     public class Sound
@@ -78,13 +89,17 @@ namespace noughts_and_crosses
 
         public Sound(string newPath)
         {
-            Path = newPath;
+            _path = newPath;
+            Uri uri = new Uri(_path, UriKind.Absolute);
+            Stream resourceStream = Application.GetResourceStream(uri).Stream;
+            player = new SoundPlayer(resourceStream);
         }
 
         public void Play()
         {
             if (App.MainApp.soundsOn)
             {
+                player.Stop();
                 player.Play();
             }
         }
